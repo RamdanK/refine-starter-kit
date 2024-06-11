ae92d6642ee597d6d3b3105ef87f6357fbc02d1c
Ramdan Kurniawan <ramdan.kurniawan@tritronik.com>
2024-06-11 14:37:36 +0700

    build: setup vitest with react testing library with simple example

---
+++ vitest.config.ts
@@ -0,0 +1,20 @@
+/// <reference types="vitest" />
+
+import { defineConfig, mergeConfig } from 'vitest/config';
+import viteConfig from './vite.config.ts';
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/tests/setup.ts',
+      css: true,
+      coverage: {
+        provider: 'v8',
+        reporter: ['html']
+      }
+    }
+  })
+);
