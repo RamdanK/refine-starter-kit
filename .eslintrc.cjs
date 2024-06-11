22b976af5eabd81fee276175d51189147db9089f
Ramdan Kurniawan <ramdan.kurniawan@tritronik.com>
2024-06-11 13:32:46 +0700

    build: setup prettier & eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,23 @@
 /* eslint-env node */
 
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, jest: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
   ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  plugins: ["react-refresh"],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh', 'prettier'],
   rules: {
-    "react-refresh/only-export-components": "warn",
+    'react-refresh/only-export-components': 'warn',
+    'react/react-in-jsx-scope': 'off'
   },
+  settings: {
+    'import/resolver': {
+      typescript: {}
+    }
+  }
 };
